50fa403c2e59c57c67673c00389a5d016947cbfb
Blake Sweeney <blakes.85@gmail.com>
2011-12-27 20:23:52 -0500

    Outline

---
+++ rna_structure_parser.py
@@ -0,0 +1,12 @@
+class RNAStructureParser(object):
+    """Docstring for RNAStructureParser """
+
+    def __init__(self):
+        """@todo: to be defined """
+
+        loops = {
+            'hairpins' = [],
+        }
+
+    def parse(self, sequence):
+        return []
