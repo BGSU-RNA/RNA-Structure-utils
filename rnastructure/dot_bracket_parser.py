9de37f978043a9f6aec66e59931b326ef66fdf5f
Blake Sweeney <blakes.85@gmail.com>
2011-12-27 21:43:40 -0500

    Rename

---
+++ rnastructure/dot_bracket_parser.py
@@ -0,0 +1,14 @@
+class DotBracketParser(object):
+    """Parse Dot Bracket Structure
+    """
+
+    def __init__(self, structure):
+        """
+        """
+
+        loops = {
+            'hairpins': []
+        }
+
+    def parse(self, sequence):
+        return []
