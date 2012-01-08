56a63aba874396bd1649d8ee615467432452df03
Blake Sweeney <blakes.85@gmail.com>
2012-01-07 20:10:48 -0500

    Remove unneeded WSDL code.

--- rnastructure/align.py
+++ rnastructure/align.py
@@ -1,5 +1,3 @@
-from suds import null
-from suds.client import Client
 from Bio import pairwise2 as nw
 
 from rnastructure.util import correlator as cor
@@ -9,20 +7,11 @@ class Align(object):
     """Class to align RNA sequences. """
 
     def __init__(self, reference=None):
-        # self._client = Client("http://www.rcsb.org/pdb/services/pdbws?wsdl")
         self.reference = reference
         self.match = 2
         self.mismatch = -1
         self.open = -0.5
         self.extend = -0.1
-
-    def __lookup(self, sequence):
-        if isinstance(sequence, dict):
-            pdb = sequence['pdb']
-            chain = sequence['chain']
-            seq = client.service.getSequenceForStructureAndChain(pdb, chain)
-            return seq
-        return sequence
 
     def align_all(sequences):
         map(self.align, sequences)
@@ -32,8 +21,7 @@ class Align(object):
         return max(alignments, key=lambda a: a['score'])
 
     def align(self, sequence):
-        seq = self.__lookup(sequence)
-        alignments = nw.align.globalms(self.reference, seq, self.match,
+        alignments = nw.align.globalms(self.reference, sequence, self.match,
                                        self.mismatch, self.open, self.extend)
         (ref, aligned, score, mis, mat) = alignments[0]
         correlations = cor.correlate(ref, aligned)
