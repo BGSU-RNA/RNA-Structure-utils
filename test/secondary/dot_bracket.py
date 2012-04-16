755e6701494e25330a4a7cc43e130156a6112f0f
Blake Sweeney <blakes.85@gmail.com>
2012-04-16 15:53:33 -0400

    Use str() to format

--- test/secondary/dot_bracket.py
+++ test/secondary/dot_bracket.py
@@ -29,59 +29,59 @@ class OuterLoopTest(unittest.TestCase):
 
 
 # class SimpleParserTest(unittest.TestCase):
-# 
+#
 #     def setUp(self):
 #         self.structure = "((..((..))....))..((...))"
 #         self.parser = Parser(self.structure)
 #         self.loops = self.parser.loops()
 #         self.flanking = self.parser.loops(flanking=True)
-# 
+#
 #     def test_hairpins(self):
 #         ans = [tuple([[6, 7]]), tuple([[20, 21, 22]])]
 #         self.assertEqual(self.loops['hairpin'], ans)
-# 
+#
 #     def test_flanking_hairpins(self):
 #         ans = [tuple([[5, 6, 7, 8]]), tuple([[19, 20, 21, 22, 23]])]
 #         self.assertEqual(self.flanking['hairpin'], ans)
-# 
+#
 #     def test_internal(self):
 #         ans = [([16, 17], []), ([2, 3], [10, 11, 12, 13])]
 #         self.assertEqual(self.loops['internal'], ans)
-# 
+#
 #     def test_flanking_internal(self):
 #         ans = [([15, 16, 17, 18], []), ([1, 2, 3, 4], [9, 10, 11, 12, 13, 14])]
 #         self.assertEqual(self.flanking['internal'], ans)
 
 
 # class NestedParserTest(unittest.TestCase):
-# 
+#
 #     def setUp(self):
 #         self.structure = "...((..(((....)))...((((...))))...))..."
 #         self.parser = Parser(self.structure)
 #         self.loops = self.parser.loops()
 #         seq = "aaaccaacccuuuugggtttccccuuuggggtttggttt"
 #         self.sequences = self.parser.parse(seq)
-# 
+#
 #     def test_hairpins(self):
 #         ans = [tuple([[10, 11, 12, 13]]), tuple([[24, 25, 26]])]
 #         self.assertEqual(self.loops['hairpin'], ans)
-# 
+#
 #     def test_internal_loops(self):
 #         ans = [([0, 1, 2], [36, 37, 38])]
 #         self.assertEqual(self.loops['internal'], ans)
-# 
+#
 #     def test_junction_loops(self):
 #         ans = [([5, 6], [17, 18, 19], [31, 32, 33])]
 #         self.assertEqual(self.loops['junction'], ans)
-# 
+#
 #     def test_extract_hairpins(self):
 #         ans = ['uuuu', 'uuu']
 #         self.assertEqual(self.sequences['hairpin'], ans)
-# 
+#
 #     def test_extract_loops(self):
 #         ans = ['aaa*ttt']
 #         self.assertEqual(self.sequences['internal'], ans)
-# 
+#
 #     def test_extract_junction(self):
 #         ans = ['aa*ttt*ttt']
 #         self.assertEqual(self.sequences['junction'], ans)
@@ -93,19 +93,19 @@ class OuterLoopTest(unittest.TestCase):
 #         self.structure = ".((.{{..((..))..((..))...}}..))."
 #         self.parser = Parser(self.structure)
 #         self.loops = self.parser.loops()
-# 
+#
 #     def test_pseudoknots(self):
 #         ans = [([4, 5], [25, 26])]
 #         self.assertEqual(self.loops['pseudoknot'], ans)
-# 
+#
 #     def test_hairpins(self):
 #         ans = [([10, 11]), ([18, 19])]
 #         self.assertEqual(self.loops['hairpins'], ans)
-# 
+#
 #     def test_internal_loops(self):
 #         ans = [([0], [31])]
 #         self.assertEqual(self.loops['internal'], ans)
-# 
+#
 #     def test_junction_loops(self):
 #         ans = [([3, 6, 7], [14, 15], [22, 23, 24, 27, 28])]
 #         self.assertEqual(self.loops['junction'], ans)
@@ -144,3 +144,6 @@ class SimpleFormatTest(unittest.TestCase):
         val = self.formatter.format()
         self.assertEqual(val, self.structure)
 
+    def test_implict_format(self):
+        val = str(self.formatter)
+        self.assertEqual(val, self.structure)
