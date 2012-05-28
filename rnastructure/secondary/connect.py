1b5e8a576ad45286b071c9028d9e572569c88621
Blake Sweeney <blakes.85@gmail.com>
2012-05-28 11:26:12 -0400

    Add a class to format a parser into a connect style string. I've also modified the parser to accept more header types.

--- rnastructure/secondary/connect.py
+++ rnastructure/secondary/connect.py
@@ -7,6 +7,58 @@ class InvalidConnectLine(Exception):
     """This exceptions indicates that a line which is not a valid connect file
     line was found.
     """
+    pass
+
+
+class Writer(basic.Writer):
+    """Format a parser as a connect file. I'm using the format of a connect
+    file as described by:
+        http://www.rnasoft.ca/strand/help.php
+    and
+        http://www.ibi.vu.nl/programs/k2nwww/static/data_formats.html
+
+    A connect file has the following format. The first line is a header line
+    of the form:
+        $l Energy = $e
+    where $l is the length of the parser and $e is the energy of the parser.
+    The energy defaults to ''. The remaning lines are all of the form:
+        $index $sequence $prev $next $pair $index
+    where $index is the current 1 based index in the structure.
+
+    $sequence is the sequence of the given index. This will default to '?',
+    which may break some parsers. It is best to set a sequence property on the
+    parser prior to writing to prevent this.
+
+    $prev is the index of the previous position, $next is the index of the next
+    position. If either index is less than 1 or greater than the length it is
+    represented as 0.
+
+    $pair is the index this position pairs with. Positions which do not pair
+    are given as 0 here.
+    """
+
+    def format(self, parser):
+        """Format the parser into a single string containing all the contents
+        of a connect file.
+
+        :parser: The parser to format.
+        """
+        header = '%s Energy = %s\n' % (len(parser), parser.energy)
+        formatted = [header]
+        for index in xrange(len(parser)):
+            curr = index + 1
+            after = curr + 1
+            if curr >= len(parser):
+                after = 0
+            sequence = parser.sequence[index] or '?'
+            pair = parser._pairs[index]
+            if pair == None:
+              pair = -1
+            pair += 1
+            data = (curr, sequence, index, after, pair, curr)
+            line = '%s\t%s\t%s\t%s\t%s\t%s\n' % data
+            formatted.append(line)
+        return ''.join(formatted)
 
 
 class Parser(basic.Parser):
@@ -16,7 +68,7 @@ class Parser(basic.Parser):
 
     def __init__(self, lines):
         self.sequence = []
-        self.header = re.compile('\A\d+\s+dG\s*=\s*')
+        self.header = re.compile('\A\d+\s+(dG|Energy|ENERGY)')
         self.entry = \
           re.compile('\A(\d+)\s+([A-z]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s*')
         pairs = self.__pairs(lines)
--- test/secondary/connect_test.py
+++ test/secondary/connect_test.py
@@ -1,6 +1,11 @@
 import unittest
 
+from StringIO import StringIO
+
+from rnastructure.secondary import dot_bracket as DB
+
 from rnastructure.secondary.connect import Parser
+from rnastructure.secondary.connect import Writer
 from rnastructure.secondary.connect import InvalidConnectLine
 
 
@@ -20,3 +25,30 @@ class MultiStructureConnectTest(unittest.TestCase):
     def test_parses_first_only(self):
         ans = [([9, 10, 11, 12, 13, 14, 15, 16],)]
         self.assertEqual(self.loops['hairpin'], ans)
+
+
+class ConnectWriterTest(unittest.TestCase):
+    def setUp(self):
+        dot_parser = DB.Parser('((..))')
+        dot_parser.sequence = 'ccaagg'
+        self.writer = Writer()
+        self.connect = self.writer.format(dot_parser)
+
+    def test_formats_correctly(self):
+        ans = ['6 Energy = ', 
+               '1\tc\t0\t2\t6\t1',
+               '2\tc\t1\t3\t5\t2',
+               '3\ta\t2\t4\t0\t3',
+               '4\ta\t3\t5\t0\t4',
+               '5\tg\t4\t6\t2\t5',
+               '6\tg\t5\t0\t1\t6',
+               '']
+        val = self.connect.split('\n')
+        self.assertEqual(val, ans)
+
+    def test_roundtrip(self):
+        lines = StringIO(self.connect)
+        parser = Parser(lines)
+        val = parser.indices()['hairpin']
+        ans = [([2, 3],)]
+        self.assertEqual(val, ans)
