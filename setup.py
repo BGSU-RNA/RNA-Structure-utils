7545b6f4d52e431ef1db12a56bd0d962fc4e5b16
Blake Sweeney <blakes.85@gmail.com>
2013-03-07 09:19:46 -0500

    Update setup.py
    This removes the unneeded install_requires and lists all packages that are part
    of rnastructure so we actually install all packages.

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     version='0.0.10',
     author='Blake A. Sweeney',
     author_email='bsweene@bgsu.edu',
-    packages=['rnastructure'],
+    packages=['rnastructure', 'rnastructure.primary', 'rnastructure.secondary',
+              'rnastructure.util'],
     url='',
     license='LICENSE.txt',
     description='Some tools to parse RNA Structure',
     long_description=open('README.mkd').read(),
-    install_requires=[
-    ],
 )
