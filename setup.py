4e0e76f53172f20b01d629fd92623a2a69656ee6
Blake Sweeney <blakes.85@gmail.com>
2013-03-07 09:20:55 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='RNAStructure',
-    version='0.0.10',
+    version='0.0.11',
     author='Blake A. Sweeney',
     author_email='bsweene@bgsu.edu',
     packages=['rnastructure', 'rnastructure.primary', 'rnastructure.secondary',
