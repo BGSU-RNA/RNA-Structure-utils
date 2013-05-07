132e068275e9eab7877d90000a58e1c906208421
Blake Sweeney <blakes.85@gmail.com>
2013-05-07 16:20:24 -0400

    Add a wrapper around PDB's PDBx package.
    This is a wrapper around the PDBx parser to make it easier to deal with CIF
    files. The parser they give is not very pythonic so I made this to make the
    code easier to write. Plus there are some odd behaviors their code hos I'm
    trying to cover up.

---
+++ rnastructure/tertiary/cif.py
@@ -0,0 +1,118 @@
+""" This package contains a simple wrapper around the PDBx tools provided by
+PDB.  It is intended to provide a simple and pythonic way to interface with
+mmCIF data.
+"""
+
+import re
+from itertools import islice
+
+from pdbx.reader.PdbxParser import PdbxReader as Reader
+
+
+class MissingBlockException(Exception):
+    """This class is raised when trying to get a missing block of data.
+    """
+    pass
+
+
+class MissingColumn(Exception):
+    """This is raised when trying to get a missing column from a table.
+    """
+    pass
+
+
+class CIF(object):
+    """Top level container for all CIF related data. This assumes that each
+    mmCIF file contains a single datablock. This doesn't have to be true but
+    makes things easier.
+    """
+
+    def __init__(self, handle):
+        reader = Reader(handle)
+        self.data = []
+        reader.read(self.data)
+        self.data = self.data[0]
+
+    def table(self, name):
+        block_name = re.sub('^_', '', name)
+        block = self.data.getObj(block_name)
+        if not block:
+            raise MissingBlockException("Unknown block " + name)
+        return Table(block)
+
+    def __getattr__(self, name):
+        try:
+            return self.table(name)
+        except:
+            raise AttributeError("Unknown block " + name)
+
+
+class Table(object):
+    """Container for a single table in the data block. This provides some
+    useful methods for accessing the data.
+    """
+
+    def __init__(self, block):
+        self.block = block
+        self.name = self.block.getName()
+        self.columns = self.block.getItemNameList()
+        self.columns = [re.sub('_.+\.', '', name) for name in self.columns]
+
+    def row(self, number):
+        """Get a row by index. Note that this may or may not be in the same
+        order as they appear in the cif file, since cif files are not required
+        to be ordered. The row will be a dict of the form { attribute: value }.
+        Each attribute will have the name of the block stripped.
+        """
+        if number >= len(self):
+            raise IndexError("Row index out of range.")
+
+        return dict(zip(self.columns, self.block.getRow(number)))
+
+    def rows(self):
+        """Get a list of all rows"""
+        for index in xrange(len(self)):
+            yield self.row(index)
+
+    def column(self, name):
+        """Get a column by name"""
+        if name not in self.columns:
+            raise MissingColumn("Unknown column.")
+
+        values = []
+        for row in self.rows():
+            values.append(row[name])
+        return values
+
+    def size(self):
+        """Get a tuple of (rowCount, columnCount).
+        """
+        return (len(self), len(self.columns))
+
+    def __getattr__(self, name):
+        """Get the column with the given name.
+        """
+        try:
+            return self.column(name)
+        except:
+            raise AttributeError("Unknown column")
+
+    def __getitem__(self, index):
+        """Get the row with the given index.
+        """
+        if isinstance(index, int):
+            return self.row(index)
+        if isinstance(index, str):
+            try:
+                return self.column(index)
+            except:
+                raise KeyError("Unknown column " + index)
+        if isinstance(index, slice):
+            iterator = islice(self.rows(), index.start, index.stop, index.step)
+            return list(iterator)
+        raise TypeError("Unknown key type, should be str or int")
+
+    def __len__(self):
+        """Get the number of rows.
+        """
+        return self.block.getRowCount()
---
+++ test/tertiary/cif_test.py
@@ -0,0 +1,162 @@
+import unittest
+
+from rnastructure.tertiary.cif import CIF
+from rnastructure.tertiary.cif import MissingColumn
+from rnastructure.tertiary.cif import MissingBlockException
+
+with open('test/files/1FAT.cif', 'rb') as raw:
+    DATA = CIF(raw)
+
+
+class SimpleCIFTest(unittest.TestCase):
+    def setUp(self):
+        self.cif = DATA
+
+    def test_gets_table_with_leading_underscore(self):
+        ans = 'pdbx_poly_seq_scheme'
+        val = self.cif.table('_' + ans)
+        self.assertEqual(val.name, ans)
+
+    def test_gets_table_without_leading_underscore(self):
+        ans = 'pdbx_poly_seq_scheme'
+        val = self.cif.table(ans)
+        self.assertEqual(val.name, ans)
+
+    def test_attribute_gives_table(self):
+        ans = 'pdbx_poly_seq_scheme'
+        val = self.cif.pdbx_poly_seq_scheme
+        self.assertEqual(val.name, ans)
+
+    def test_fails_getting_unknown_table(self):
+        with self.assertRaises(MissingBlockException):
+            self.cif.table('bob')
+
+    def test_raises_key_when_getting_missing_attribute(self):
+        with self.assertRaises(AttributeError):
+            self.cif.bob
+
+
+class SimpleTableTest(unittest.TestCase):
+    def setUp(self):
+        self.data = DATA.table('pdbx_poly_seq_scheme')
+
+    def test_gets_all_columns(self):
+        val = self.data.columns
+        ans = ['asym_id', 'entity_id', 'seq_id', 'mon_id', 'ndb_seq_num',
+               'pdb_seq_num', 'auth_seq_num', 'pdb_mon_id', 'auth_mon_id',
+               'pdb_strand_id', 'pdb_ins_code', 'hetero']
+        self.assertEquals(val, ans)
+
+    def test_len_is_row_count(self):
+        val = len(self.data)
+        ans = 1008
+        self.assertEqual(val, ans)
+
+    def test_gets_size(self):
+        ans = (1008, 12)
+        val = self.data.size()
+        self.assertEqual(val, ans)
+
+    def test_gets_a_row(self):
+        ans = {
+            'asym_id': 'A',
+            'entity_id': '1',
+            'seq_id': '1',
+            'mon_id': 'SER',
+            'ndb_seq_num': '1',
+            'pdb_seq_num': '1',
+            'auth_seq_num': '1',
+            'pdb_mon_id': 'SER',
+            'auth_mon_id': 'SER',
+            'pdb_strand_id': 'A',
+            'pdb_ins_code': '.',
+            'hetero': 'n'
+        }
+        val = self.data.row(0)
+        self.assertEqual(val, ans)
+
+    def test_fails_getting_too_large_row(self):
+        with self.assertRaises(IndexError):
+            self.data.row(9000)
+
+    def test_iterates_over_all_rows(self):
+        ans = 1008
+        val = len(list(self.data.rows()))
+        self.assertEqual(val, ans)
+
+    def test_gets_a_column(self):
+        ans = ['A', 'B', 'C', 'D']
+        val = sorted(list(set(self.data.column('asym_id'))))
+        self.assertEqual(val, ans)
+
+    def test_fails_getting_missing_column(self):
+        with self.assertRaises(MissingColumn):
+            self.data.column('bob')
+
+    def test_get_item_can_give_row(self):
+        ans = {
+            'asym_id': 'A',
+            'entity_id': '1',
+            'seq_id': '1',
+            'mon_id': 'SER',
+            'ndb_seq_num': '1',
+            'pdb_seq_num': '1',
+            'auth_seq_num': '1',
+            'pdb_mon_id': 'SER',
+            'auth_mon_id': 'SER',
+            'pdb_strand_id': 'A',
+            'pdb_ins_code': '.',
+            'hetero': 'n'
+        }
+        val = self.data[0]
+        self.assertEqual(val, ans)
+
+    def test_get_item_can_give_range(self):
+        ans = [{'asym_id': 'A',
+                'entity_id': '1',
+                'seq_id': '1',
+                'mon_id': 'SER',
+                'ndb_seq_num': '1',
+                'pdb_seq_num': '1',
+                'auth_seq_num': '1',
+                'pdb_mon_id': 'SER',
+                'auth_mon_id': 'SER',
+                'pdb_strand_id': 'A',
+                'pdb_ins_code': '.',
+                'hetero': 'n'},
+               {'asym_id': 'A',
+                'entity_id': '1',
+                'seq_id': '2',
+                'mon_id': 'ASN',
+                'ndb_seq_num': '2',
+                'pdb_seq_num': '2',
+                'auth_seq_num': '2',
+                'pdb_mon_id': 'ASN',
+                'auth_mon_id': 'ASN',
+                'pdb_strand_id': 'A',
+                'pdb_ins_code': '.',
+                'hetero': 'n'}]
+        val = self.data[0:2]
+        self.assertEqual(val, ans)
+
+    def test_get_item_can_give_column(self):
+        ans = ['A', 'B', 'C', 'D']
+        val = sorted(list(set(self.data['asym_id'])))
+        self.assertEqual(val, ans)
+
+    def test_dot_gives_column(self):
+        ans = ['A', 'B', 'C', 'D']
+        val = sorted(list(set(self.data.asym_id)))
+        self.assertEqual(val, ans)
+
+    def test_get_item_on_missing_string_gives_key(self):
+        with self.assertRaises(KeyError):
+            self.data['bob']
+
+    def test_dot_on_missing_column_gives_attribute(self):
+        with self.assertRaises(AttributeError):
+            self.data.bob
+
+    def test_get_item_on_too_big_int_gives_index(self):
+        with self.assertRaises(IndexError):
+            self.data[90000]
