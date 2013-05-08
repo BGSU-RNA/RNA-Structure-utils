f458c7815d6f28c0cb48ca6623fc04fc485e06aa
Blake Sweeney <blakes.85@gmail.com>
2013-05-08 11:21:26 -0400

    Update fabfile with deploy task.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,15 @@
 from fabric.api import local, task
 
+
 @task(default=True)
 def test():
     local("nosetests test/**/*.py")
+
+
+@task
+def deploy():
+    local("git push origin develop")
+    local("git checkout master")
+    local("git merge master develop")
+    local("git push origin master")
+    local("git checkout develop")
